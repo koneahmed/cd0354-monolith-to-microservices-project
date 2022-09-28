f34c53be950e435b5525a3485aba201dc193c5da
Ahmed-Yulcom <93929978+Ahmed-Yulcom@users.noreply.github.com>
2022-09-28 20:54:38 +0000

    add new

--- myscript.sh
+++ myscript.sh
@@ -1,8 +1,17 @@
+kubectl apply -f env-configmap.yml
+kubectl apply -f env-secrets.yml
+
+
 
 kubectl delete deployment feed
 kubectl delete deployment user
 kubectl delete deployment frontend
 kubectl delete deployment reverseproxy
+
+
+kubectl delete deployment backend-feed
+kubectl delete deployment backend-user
+
 
 kubectl apply -f user-deployment.yml
 kubectl apply -f feed-deployment.yml
