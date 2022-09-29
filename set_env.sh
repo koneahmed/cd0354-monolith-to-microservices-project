5109547a8ac704be6d4ecc2aafe703d2d7019a06
Ahmed-Yulcom <93929978+Ahmed-Yulcom@users.noreply.github.com>
2022-09-29 11:15:47 +0000

    Update set_env.sh

--- set_env.sh
+++ set_env.sh
@@ -1,14 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=udagrammicro
-export POSTGRES_PASSWORD=udagrammicro
-export POSTGRES_HOST=udagrammicro.cvwfc2ch6nzl.us-east-2.rds.amazonaws.com
-export POSTGRES_DB=udagrammicro
-export AWS_BUCKET=udragramprojet3475659817827
-export AWS_REGION=us-east-2
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=mypassword
+export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
-# S3 s3-udgram-monolothic-to-microservice-475659817827
-    # // "typescript": "^3.9.10"
-
